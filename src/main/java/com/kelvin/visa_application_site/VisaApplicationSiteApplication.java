a3838d2127cd4bb8dcafae9a67944c139d448ddf
Kelvin Justine <justineikechi6@gmail.com>
2025-09-01 22:47:56 +0100

    first commit

---
+++ src/main/java/com/kelvin/visa_application_site/VisaApplicationSiteApplication.java
@@ -0,0 +1,13 @@
+package com.kelvin.visa_application_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisaApplicationSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisaApplicationSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kelvin/visa_application_site/VisaApplicationSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kelvin.visa_application_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisaApplicationSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
